949f5621b5f84f9e6578688415a8ac325128cc96
Kailramiya <amankundu369@gmail.com>
2025-11-04 00:07:21 +0530

    fix:: put credentials false

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const allowedOrigins = ['*'];
 
 const corsOptions = {
   origin: allowedOrigins,
-  credentials: true,
+  // credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   optionsSuccessStatus: 200,
